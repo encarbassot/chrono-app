64b858f8b2e27916eac6dab5805c335b92a9876b
encarbassot <elgrefa@gmail.com>
2024-10-21 20:45:21 +0200

    base url

--- chrono-app-frontend/vite.config.js
+++ chrono-app-frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { BASE_URL } from './config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: BASE_URL,
   plugins: [react()],
 })
